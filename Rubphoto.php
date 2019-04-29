055eff0bb6378dea1bf49ec40995c938e16dc398
jcollart <j.collart@simplon-charleville.fr>
2019-04-29 16:33:59 +0200

    creation rubphotos

---
+++ Rubphoto.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php include('header.php');?>
+
+
+    <!--------------------------------------------------------------------------------
+    -                        HEADER RUBRIQUE PHOTOS                                 -
+    --------------------------------------------------------------------------------->
+
+    <div id="header_rubphotos">
+        <div id="cadre_titre">
+            <h1>RUBRIQUE PHOTOS</h1>
+        </div>
+    </div>
+    
+
+
+
+
+
+
+
+
+<?php include('footer1.php');?>
+<?php include('footer2.php');?>    
+</body>
+</html>
