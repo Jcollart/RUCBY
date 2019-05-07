e61e14f35879b42737b9ede128975f9b6bf7c9f9
1 <g.ponsard@simplon-charleville.fr>
2019-05-07 16:32:38 +0200

    modif style news

--- news.php
+++ news.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,8 +8,9 @@
     <link rel="stylesheet" href="css/news.css">
     <title>Les News</title>
 </head>
+
 <body>
-<?php include('header2.php'); ?>
+    <?php include('header.php'); ?>
 
     <!--HEADER NEWS-->
 
@@ -17,12 +19,68 @@
             <h1>LES NEWS</h1>
         </div>
     </div>
-<!---FIN <div id="header_contact"> --->
+
+
+    <!---TEXTE PRESENTATION DE LA RUBRIQUE NEWS ---->
+
+    <div id=conteneur_texte>
+        <p>Lorem ipsum dolordrt sit amet consectetur adipisicing elit. Illo ipsa, quas expedita sint, impedit ex earum
+            consequatur repudianda nesciunt labore dolore quo suscipit reiciendis, rem debitis sed atque! Quas, et!
+        </p>
+    </div>
+
+    <!--- ARTICLE NEWS --->
+    <article id=news>
+
+        <div id=image_article><img src="images/futsal.jpg.jpg"></div>
+        <div id=contenu_article>
+            <h1>Titre article</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, sapiente. Blanditiis, numquam
+                cupiditate! Magnam inventore
+                praesentium quibusdam. Voluptatum tempore suscipit cumque doloremque deserunt possimus vero nesciunt
+                incidunt voluptates rem. Veritatis?<br>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum repudiandae exercitationem veniam non?
+                Vitae itaque doloremque, sunt sint nam sapiente optio, facilis dolorem ab quam aliquid id explicabo
+                earum non.
+                
+            </p>
+        </div>
+
+    </article>
+    
+    <article id=news>
+
+        
+        <div id=contenu_article>
+            <h1>Titre article</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, sapiente. Blanditiis, numquam
+                cupiditate! Magnam inventore
+                praesentium quibusdam. Voluptatum tempore suscipit cumque doloremque deserunt possimus vero nesciunt
+                incidunt voluptates rem. Veritatis?<br>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum repudiandae exercitationem veniam non?
+                Vitae itaque doloremque, sunt sint nam sapiente optio, facilis dolorem ab quam aliquid id explicabo
+                earum non.
+                
+            </p>
+        </div>
+        <div id=image_article><img src="images/futsal.jpg.jpg"></div>
+
+    </article>
+
+    
+
+
+
+    <!--- ARTICLE NEWS --->
 
 
 
 
-    <?php include('footer.php'); ?>
+
+
+    <?php include('footer1.php'); ?>
+    <?php include('footer2.php'); ?>
 
 </body>
+
 </html>
