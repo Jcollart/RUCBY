85192fd60aed96418d1e0992c0ccbe6e79484903
jcollart <j.collart@simplon-charleville.fr>
2019-05-07 16:36:58 +0200

    modif rubphoto

--- Rubphoto.php
+++ Rubphoto.php
@@ -5,20 +5,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-  </script>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-  </script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-  </script>
-
+  
   <link rel="stylesheet" href="css/rubphotos.css">
-  <title>Document</title>
+  <title>rubrique photo</title>
 </head>
 
 <body>
@@ -30,29 +19,10 @@
   </div>
 
   <div id="choix_rubphoto">
-    <div class="card" style=<img class="card-img-top" src="images/mondial2019.jpg" alt="Card image cap">"width: 19em;">
-      <div class="card-body">
-        <h5 class="card-title">IMAGES TOURNOI</h5>
-        <p class="card-text"></p>
-        <a href="galerie.php" class="btn btn-primary">allez zone tournoi</a>
-      </div>
-    </div>
-    <div class="card" style="width: 16em;">
-      <img class="card-img-top" src="images/futsal.jpg" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">IMAGES MATCHS</h5>
-        <p class="card-text">quelques images</p>
-        <a href="galerie.php" class="btn btn-primary">allez zone match</a>
-      </div>
-    </div>
-    <div class="card" style="width: 16em;">
-      <img class="card-img-top" src="images/soireefutsal.jpg" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">IMAGES SOIREES</h5>
-        <p class="card-text"></p>
-        <a href="galerie.php" class="btn btn-primary">allez zone soirée</a>
-      </div>
-    </div>
+
+    <div id="cotegauche"><a href="galerie.php"><h1>RUBRIQUE TOURNOI</h1><img  src="images/mondial2019.jpg" width="500px" height="600px" alt=""></a></div>
+    <div id="cotemilieu"><a href="galerie.php"><h1>RUBRIQUE MATCH</h1><img  src="images/futsal.jpg" width="500px" height="600px" alt=""></a></div>
+    <div id="cotedroit"><a href="galerie.php"><h1>RUBRIQUE SOIREE</h1><img  src="images/soireefutsal.jpg" width="500px" height="600px" alt=""></a></div>
 
   </div>
  
