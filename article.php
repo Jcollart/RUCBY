2b0c21cbc9a4765e2fe0f7816366f372e16c62c1
1 <g.ponsard@simplon-charleville.fr>
2019-05-16 16:38:07 +0200

    ajouts boutons partage sur article

--- article.php
+++ article.php
@@ -5,16 +5,16 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  
+
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
     integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/font-awesome-animation.min.css">
-  
+  <link rel="stylesheet" href="css/font-awesome-animation.min.css">
 
 
 
 
-    <link rel="stylesheet" href="css/article.css">
+
+  <link rel="stylesheet" href="css/article.css">
 
   <title>Article</title>
 </head>
@@ -70,17 +70,19 @@ $requete->closeCursor(); // Termine le traitement de la requête
 ?>
 
   <div class=boutons_partage>
-    <div id=partage_fb ><a class="faa-parent animated-hover" href="https://www.facebook.com/sharer.php?u=<?= $lien ?>&t=<?= $titre ?>"
+    <div id=partage_fb><a class="faa-parent animated-hover"
+        href="https://www.facebook.com/sharer.php?u=<?= $lien ?>&t=<?= $titre ?>"
         onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=700');return false;">
         <i class="fab fa-facebook-square fa-5x faa-shake"></i>
       </a>
     </div>
-
+    <div id=partage_tw><a class="faa-parent animated-hover"
+        href="https://twitter.com/share?url=<?= $titre ?>&via=votre-nom-twitter"
+        onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=700');return false;">
+        <i class="fab fa-twitter-square fa-5x faa-shake"></i>
+      </a>
+    </div>
   </div>
-
-
-
-
 
 
   <?php include('footer.php'); ?>
