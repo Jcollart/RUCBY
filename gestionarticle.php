0da042aba49fa1c934df53c3f5f8eb6e4b0ec285
jcollart <j.collart@simplon-charleville.fr>
2019-05-22 13:21:11 +0200

    requete delete

--- gestionarticle.php
+++ gestionarticle.php
@@ -2,7 +2,7 @@
 try
 {
   // On se connecte à MySQL
-  $bdd = new PDO('mysql:host=localhost;dbname=futsal;charset=UTF8', 'commun', 'commun');
+  $bdd = new PDO('mysql:host=localhost;dbname=futsal;charset=UTF8', 'root', '');
   
 }
 catch(Exception $e)
--- supprimer.php
+++ supprimer.php
@@ -14,9 +14,10 @@ catch(Exception $e)
 // Si tout va bien, on peut continuer
 if(isset($_GET['id_news']) AND !empty($_GET['id_news'])) {
     $suppr_id = htmlspecialchars($_GET['id_news']);
-    $suppr = $bdd->prepare('DELETE id_news FROM news  WHERE id_news');
+    $suppr = $bdd->prepare('DELETE FROM news  WHERE id_news=?');
     $suppr->execute(array($suppr_id));
-    header('Location: http://127.0.0.1/html/rucby/');
+    echo ' la news est supprimée youpi';
+    
  }
-  echo ' la news est supprimée';
+ 
  ?>
