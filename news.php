5e2fcf3c8c188e351952c2c087cfdf2ba64c79ff
1 <g.ponsard@simplon-charleville.fr>
2019-05-27 09:44:21 +0200

    rectif news

--- news.php
+++ news.php
@@ -33,12 +33,6 @@
       consequatur repudianda nesciunt labore dolore quo suscipit reiciendis, rem debitis sed atque! Quas, et!
     </p>
   </div>
-
-  
-  
-  <!--- ARTICLE NEWS --->
-  <article id=news>
-  
   <?php
   
   $requete = $bdd->prepare  ("SELECT nom_image, titre_news, SUBSTRING(description_news, 1, 300) as description_news FROM appartenir, news, image WHERE news.id_news = appartenir.id_news AND image.id_image = appartenir.id_image AND news.id_news=image.id_image ORDER BY news.date_news DESC limit 0, 3");
@@ -49,27 +43,29 @@
   
   
   ?>
+  
+  
+  <!--- ARTICLE NEWS --->
+  <article id=news>
+  
+  
 
         <div id=image_article><a href="article.php?id_news=<?php echo $donnees['$id'];?>"><img src="images/<?php echo $donnees['nom_image']; ?>" ></a></div>
         <div id=contenu_article>
             <h1><?php echo $donnees['titre_news']; ?></h1>
-
-            
+         
             <p><?php echo $donnees['description_news']; ?>...</p>
 
-
+        </div>
+      
+    
+            </article>
             <?php
   }
  
 $requete->closeCursor(); // Termine le traitement de la requête
 
 ?>
-
-        </div>
-      
-    
-            </article>
-      
      
 
 
