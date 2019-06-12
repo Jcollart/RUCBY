2e38ad26433a6a473a887173ca6ede7d079e5f4d
bologia <g.gusberti@simplon-charleville.fr>
2019-06-12 16:09:37 +0200

    adios pagos

--- supprimerphoto.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(isset($_GET['suppr']) AND !empty($_GET['suppr'])) {
-    $suppr = $bdd->prepare('DELETE FROM photo WHERE id_photo=?');
-    $suppr->execute(array($_GET['suppr']));
- }
- ?>
