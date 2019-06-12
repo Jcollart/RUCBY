b573bd1611988ff9ec9f730c614188432872ff54
bologia <g.gusberti@simplon-charleville.fr>
2019-06-12 16:07:53 +0200

    la vie ça marche sa mère merci teddy !!!!

--- supprimerphoto.php
+++ supprimerphoto.php
@@ -1,22 +1,6 @@
 <?php
-try
-{
-  // On se connecte à MySQL
-  $bdd = new PDO('mysql:host=localhost;dbname=futsal;charset=UTF8', 'commun', 'commun');
-
-}
-catch(Exception $e)
-{
-
-        die('Erreur : '.$e->getMessage());
-}
-
-if(isset($_GET['id_photo']) AND !empty($_GET['id_photo'])) {
-    $suppr_id = htmlspecialchars($_GET['id_photo']);
+if(isset($_GET['suppr']) AND !empty($_GET['suppr'])) {
     $suppr = $bdd->prepare('DELETE FROM photo WHERE id_photo=?');
-    $suppr->execute(array($suppr_id));
-  //  header('Location: gestionphoto.php'); //
-    exit;
+    $suppr->execute(array($_GET['suppr']));
  }
-
  ?>
